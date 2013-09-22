6b9fc0e17fb13dfe34992c013e134168a2b31b08
Gareth Sciffer <sciff90@gmail.com>
2013-09-23 07:59:58 +1000

    First Commit

---
+++ rand_chain/src/rand_chain.cu
@@ -0,0 +1,40 @@
+#include <thrust/random.h>
+#include <thrust/device_vector.h>
+#include <thrust/transform.h>
+#include <thrust/iterator/counting_iterator.h>
+#include <iostream>
+#include <fstream>
+struct prg
+{
+	float a, b;
+
+	__host__ __device__ 
+		prg(float _a=0.f, float _b=1.f) : a(_a), b(_b) {};
+	__host__ __device__ 
+		float operator()(const unsigned int n) const
+        {
+		thrust::default_random_engine rng;
+		thrust::random::experimental::normal_distribution<float> dist(a, b);
+		rng.discard(n);
+
+		return dist(rng);
+	}
+};
+
+
+int main(void)
+{
+	    const int N = 100000;
+	    thrust::device_vector<float> numbers(N);	    
+	    thrust::counting_iterator<unsigned int> index_sequence_begin(0);
+	    thrust::transform(index_sequence_begin,index_sequence_begin + N,numbers.begin(),prg(0.f,1.f));
+	    
+	    std::ofstream myfile;
+	    myfile.open("../data/normal.dat");
+	    for(int i = 0; i < N; i++)
+	    {
+		    myfile << numbers[i] << std::endl;
+	    }
+	    myfile.close();
+	    return 0;
+} 
